097ca317b6d2917c51e3eb0e84cce9499f7486bd
Alex Donovan <aldonovan@micds.org>
2024-02-24 17:39:10 -0800

    Math

---
+++ CSES/josephus_queries.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int CalculateAnswer(const int n, const int k) {
+  if (n == 1) {
+    return 0;
+  }
+  if (2 * k <= n) {
+    return 2 * k - 1;
+  }
+  int index = CalculateAnswer(n - n / 2, k - n / 2);
+  if (index == 0 && n % 2 != 0) {
+    return n - 1;
+  }
+  if (n % 2 != 0) {
+    --index;
+  }
+  return 2 * index;
+}
+
+int main() {
+  int q;
+  std::cin >> q;
+  for (int i = 0; i < q; ++i) {
+    int n, k;
+    std::cin >> n >> k;
+    std::cout << CalculateAnswer(n, k) + 1 << '\n';
+  }
+}
